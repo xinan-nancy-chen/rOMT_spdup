808c209c995a18c3b1da75101b411eb3f181cded
Xinan (Nancy) Chen <80916715+xinan-nancy-chen@users.noreply.github.com>
2021-04-26 16:38:35 -0400

    Add files via upload

--- utilities/sdiag.m
+++ utilities/sdiag.m
@@ -1,3 +1,6 @@
 function [S] = sdiag(d)
 % Returns d on the main diag of a sparse matrix.
-S = spdiags(d(:),0,numel(d),numel(d));
+%S = spdiags(d(:),0,numel(d),numel(d));
+k = numel(d);
+S = sparse(1:k,1:k,d(:),k,k);
+end
