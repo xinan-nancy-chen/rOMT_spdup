46811d6bf8ab5b4122143e787e56679c5b3d2645
Xinan (Nancy) Chen <80916715+xinan-nancy-chen@users.noreply.github.com>
2022-01-19 22:04:29 -0500

    Update paramInitGLADpar2.m

--- paramInitGLADpar2.m
+++ paramInitGLADpar2.m
@@ -1,9 +1,6 @@
 function glacfg = paramInitGLADpar2(cfg)
 % Created by Xinan Chen on 04/26/2021.
-% This function returns parameters corresponding to the input 'data_name' and analysis type
-%   Input:  type  := 's' if run for speed map; 'v' if run for flux vectors
-%   Output: 'glad_par' := structure containing parameters for GLAD analysis
-
+% This function returns parameters corresponding to the input 'data_name'
 %%
 
 glacfg.do_masked = 1;%1 if ensure everything is inside mask before saving to .nii
@@ -77,8 +74,69 @@ switch cfg.dataset_name
                 
 
     otherwise
-        fprintf('paramInitGLADpar: non-applicable dataset_name!');
-        return
+        %fprintf('paramInitGLADpar: non-applicable dataset_name!');
+        %return
+        glacfg.do_sp = 1;%0; %1 if use sp from max(data)>sp_thresh, 0 o/w
+        glacfg.sp_thresh = 0.2;%1;%0;
+
+        glacfg.mdt = 1;
+        glacfg.sl_tol = 3; %threshold for minimum Euclidean length between initial and final streamline points                
+        glacfg.spType = 'ordered'; %'uniform' (default: 'ordered'); %'ordered': every fs-th possible sp is used (default)sp distribution; 'uniform': sp chosen according to uniform distribution
+        switch glacfg.spType
+            case 'ordered'
+                glacfg.spfs = 1;
+                glacfg.spSTR = sprintf('spDISTordered_fs%d',glacfg.spfs);
+                glacfg.spSTRtitle = sprintf('spDISTordered-fs%d',glacfg.spfs);
+            case 'uniform'
+                glacfg.spPerc = 40;
+                glacfg.spSTR = sprintf('spDISTunif_spPerc%d',glacfg.spPerc);
+                glacfg.spSTRtitle = sprintf('spDISTunif-spPerc%d',glacfg.spPerc);
+        end
+        glacfg.nEulStep = 1; %number of Eulerian steps to be taken
+        
+        glacfg.smoothv = 0; % smooth velocity field
+        if glacfg.smoothv
+            glacfg.Svt = 5;%10; % smoothing w.r.t time
+            glacfg.Svs = 1;%5; % smoothing w.r.t space
+            glacfg.smoothvSTR = sprintf('1_Tolt%d_Tols%d',glacfg.Svt,glacfg.Svs);
+        else
+            glacfg.smoothvSTR = '0';
+        end
+        
+        glacfg.smoothp = 0;%1; % smooth pathline
+        if glacfg.smoothp
+            glacfg.smpTol = 100; % the higher, the smoother the pathlines
+            glacfg.smoothpSTR = sprintf('1_Tol%d',glacfg.smpTol);
+        else
+            glacfg.smoothpSTR = '0';
+        end
+        
+        glacfg.sfs = 5;
+        glacfg.vfs = 1;
+
+
+        % NCA parameters
+        glacfg.radius = 2; % redius to dialate
+        glacfg.NNnum_tol = 20; 
+        glacfg.stdcut = 0.5; 
+        glacfg.meancut = [0.5,1]; 
+        glacfg.Npcut = 1; 
+        glacfg.Avepathlcut = 8; 
+        glacfg.threshstr = sprintf('NNnum_tol_%d_||_stdcut_%.2f_wmeancut_[%.2f,%.2f]_Npcut_%d_Avepathlcut_%.1f',glacfg.NNnum_tol,glacfg.stdcut,glacfg.meancut(1),glacfg.meancut(2),glacfg.Npcut,glacfg.Avepathlcut);
+
+        glacfg.maskdilate = 3;
+        glacfg.radius2 = 3; % redius to dialate
+        glacfg.NNnum_tol2 = 10; 
+        glacfg.stdcut2 = 0.4; 
+        glacfg.meancut2 = [0.6,1]; 
+        glacfg.Npcut2 = 1; 
+        glacfg.Avepathlcut2 = 10; 
+        glacfg.pathlcut2 = 20;%10;%20;
+        glacfg.threshstr2 = sprintf('NNnum_tol_%d_||_stdcut_%.2f_wmeancut_[%.2f,%.2f]_Npcut_%d_Avepathlcut_%.1f|pathlcut_%.1f',glacfg.NNnum_tol2,glacfg.stdcut2,glacfg.meancut2(1),glacfg.meancut2(2),glacfg.Npcut2,glacfg.Avepathlcut2,glacfg.pathlcut2);
+
+
+        % vis parameters
+        glacfg.jp = 5;       
 
 end
 
