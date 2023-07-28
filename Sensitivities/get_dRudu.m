cfa1bd63589cd180a48f550fb671c181662a9638
Xinan (Nancy) Chen <80916715+xinan-nancy-chen@users.noreply.github.com>
2023-07-28 11:52:09 -0400

    Update get_dRudu.m

--- Sensitivities/get_dRudu.m
+++ Sensitivities/get_dRudu.m
@@ -6,9 +6,9 @@ function dRdu=get_dRudu(u,nt,par)
 %This function returns the derivative of the regularization term
 %R(u)=||grad(u)||^2
 
-%% 2D verison
+%% 2D/3D verison
 G=par.Grad'*par.Grad;%G=-par.Grad'*par.Grad;
-U=vec2mat(u(:),2*nt);
+U=vec2mat(u(:),par.dim*nt);
 dRdu=G*U;
 dRdu=dRdu(:)';
 
